81c1e57c7d0284100681f2724780acb58ba80b74
pulkiiiit <pulkitshukla001@gmail.com>
2025-08-12 02:29:20 +0530

    Version 1

--- app/page.tsx
+++ app/page.tsx
@@ -184,9 +184,9 @@ export default function Home() {
                     <span className="text-xs px-2 py-1 rounded-full bg-white/5 text-gray-400">Shad CN</span>
                   </div>
                   <div className="flex items-center gap-3 md:gap-4 pt-2">
-                    <a href="#" target="_blank" rel="noopener norefereer" className="text-gray-400 flex items-center gap-1"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>GitHub</a>
+                    <a href="#" target="_blank" rel="noopener norefereer" className="text-gray-400 flex items-center gap-1"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>GitHub</a>
                     <a href="https://www.framefinder.co/" target="_blank" rel="noopener norefereer" className="text-gray-400 flex items-center gap-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-external-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-external-link">
                     <path d="M15 3h6v6"></path>
                     <path d="M10 14 21 3"></path>
                     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
@@ -199,7 +199,7 @@ export default function Home() {
           <div className="flex justify-center mt-12">
             <button className="flex items-center gap-2 px-4 py-2 bg-white/5 rounded-full">
             <span>Rest are coming up</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"></path></svg></button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"></path></svg></button>
           </div>
         </section>
         <section id="experience" className="py-8 px-4 md:px-6">
@@ -243,7 +243,7 @@ export default function Home() {
                     <h3 className="font-bold text-lg md:text-xl text-white leading-tight">Google Devloper Group</h3>
                     <p className="text-gray-300 font-medium mt-1 text-sm md:text-base">Event Management Lead</p>
                   </div>
-                  <div className="flex items-center gap-2 text-gray-400 text-sm md:text-base shrink-0"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-calendar text-gray-500">
+                  <div className="flex items-center gap-2 text-gray-400 text-sm md:text-base shrink-0"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-calendar text-gray-500">
                   <path d="M8 2v4"></path><path d="M16 2v4"></path><rect width="18" height="18" x="3" y="4" rx="2" /><path d="M3 10h18"></path></svg>
                   <span className="font-medium">Oct 2024 - May 2025</span></div>
                 </div>
